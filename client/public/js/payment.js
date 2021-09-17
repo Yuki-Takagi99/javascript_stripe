96479d0cfc95d58f1f82d735d274f2ab17ba6a29
yuki takagi <yuki99takagi@gmail.com>
2021-09-18 08:38:53 +0900

    wip: payment.js

--- client/public/js/payment.js
+++ client/public/js/payment.js
@@ -39,6 +39,28 @@ card.on('change', ({error}) => {
 
 const submitButton = document.getElementById("payment-form-submit");
 
+// ボタンがクリックされたら、アクションを実行
 submitButton.addEventListener("click", function(event) {
 
+  stripe
+  .createPaymentMethod("card", card)
+  .then(function(result){
+    if(result.error) {
+    } else {
+      // 支払いメソッドIDをリクエストデータに詰める
+      order.paymentMethodld = result.paymentMethodld;
+
+      // サーバーサイドへ決済情報を渡して結果をハンドリングする
+      // サーバはhttp://localhost:3000/v1/order/payment にPOSTでリクエストを受け付けている
+      fetch("http://localhost:3000/v1/order/payment", {method: "POST", headers: {"Content-Type":"application/json"}, body: JSON.stringify(order)})
+      .then(function(result){
+        return result.json(); // HTTPレスポンスからボディをJSONを取り出して次のメソッドに引き渡す
+      })
+      .then(function(response){
+        // 正常終了
+      });
+    }
+  })
+  .catch(function(){
+  });
 });
